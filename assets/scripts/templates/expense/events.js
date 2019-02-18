2dca4d6054af82aca4b7c9d71df718f1be99e4ec
fords <zxw6805@rit.edu>
2019-02-18 01:25:37 -0500

    Payment works when an expense is edited to add new person

--- assets/scripts/templates/expense/events.js
+++ assets/scripts/templates/expense/events.js
@@ -129,7 +129,9 @@ const onSelectPeopleForPayment = function (event) {
   let payTemp = 0
   store.payments_person_id = []
   for (const i in $('#listPeople3')[0].selectedOptions) {
+
     if ($('#listPeople3')[0].selectedOptions[i].value !== undefined) {
+      payTemp = 0
       store.payments[store.index_i].forEach(function (entry) {
         if (entry.person === $('#listPeople3')[0].selectedOptions[i].value) {
           payTemp = entry.pay
@@ -206,6 +208,7 @@ const onAddExpenseFormSave = function (event) {
   // add the person object selected in Add Expense option
   for (const i in $('#listPeople2')[0].selectedOptions) {
     if ($('#listPeople2')[0].selectedOptions[i].value !== undefined) {
+      payVar = 0
       store.payments[store.index_i].forEach(function (entry) {
         if (entry.person === $('#listPeople2')[0].selectedOptions[i].value) {
           payVar = entry.pay
