aa5dff953aee159dfb5a669f5d9e3c75d0b833e6
fords <zxw6805@rit.edu>
2019-02-18 23:17:50 -0500

    image styling

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,12 +6,17 @@ const authEvents = require('./templates/auth/events.js')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+function doParallax () {
+  const positionY = window.pageYOffset / 2
+  document.body.style.backgroundPosition = '0 -' + positionY + 'px'
+}
 
 $(() => {
   // your JS code goes here
 
   // event handler
   events.addHandlers()
+  window.addEventListener('scroll', doParallax)
   $('#sign-up').on('submit', authEvents.onSignUp)
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#sign-out').on('submit', authEvents.onSignOut)
